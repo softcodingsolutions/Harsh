2f79e793886211beb453a7545691d53cc689a9c1
harsh291199 <harshsuthar2911@gmail.com>
2024-05-01 17:05:44 +0530

    Add condition for non-string argument

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -1,6 +1,17 @@
 require_relative '../string_calc'
 
+describe StringCalc do
+  describe '#add' do
+    context 'when passed a non-string input' do
+      it 'raises ArgumentError with correct message' do
+        expect { described_class.add('5') }.not_to raise_error(ArgumentError, 'Invalid input! Please add only string')
+      end
+    end
 
-describe StringCalculator do
-
+    context "when passed a string input" do
+      it 'does not raise any error' do
+        expect { described_class.add(142) }.to raise_error(ArgumentError, 'Invalid input! Please add only string')
+      end
+    end
+  end
 end
--- string_calc.rb
+++ string_calc.rb
@@ -1,3 +1,5 @@
 class StringCalc
-
+  def self.add(numbers)
+    raise ArgumentError, 'Invalid input! Please add only string' unless numbers.is_a?(String)
+  end
 end
